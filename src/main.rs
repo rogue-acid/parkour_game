b5c04682f5a6c8ed9cd8fdd3ed1a6b982007bec4
0xadk <0xadk@users.noreply.github.com>
2021-11-27 12:33:02 -0800

    update game to use scenes

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,22 @@
+use std::collections::HashMap;
+
 use raylib::prelude::*;
 
-struct Controls {
-	move_up: KeyboardKey,
-	move_down: KeyboardKey,
-	move_left: KeyboardKey,
-	move_right: KeyboardKey,
-	jump: KeyboardKey,
+mod scenes;
+
+struct GameState {
+	current_scene: SceneType,
+	assets: HashMap<String, Texture2D>
 }
 
-struct Player {
-	pos: Vector2,
-	width: f32,
-	height: f32,
-	color:  Color,
-	velocity: Vector2,
-	controls: Controls,
-	movement_speed: f32,
-	jump_power: f32,
+enum SceneType {
+	Game,
 }
 
-struct Clothes {
-	model: Vec<Vector2>,
-	offset: Vector2,
-}
-
-fn draw_rectangle(player: &Player, handle: &mut raylib::core::drawing::RaylibDrawHandle) {
-	handle.draw_rectangle(
-		player.pos.x as i32,
-		player.pos.y as i32,
-		player.width as i32,
-		player.height as i32,
-		player.color,
-	);
-}
-
-fn handle_player_movement(player: &mut Player, delta: f32, handle: &mut raylib::core::drawing::RaylibDrawHandle) {
-	if handle.is_key_pressed(player.controls.jump) {
-		player.velocity.y = -player.jump_power * delta
-	}
-	if handle.is_key_down(player.controls.move_left) {
-		player.pos.x -= player.movement_speed * delta
-	}
-	if handle.is_key_down(player.controls.move_right) {
-		player.pos.x += player.movement_speed * delta
-	}
+trait Scene {
+	fn init(&mut self);
+	fn update(&mut self, d: &mut RaylibDrawHandle, delta: f32);
+	fn display(&mut self, d: &mut RaylibDrawHandle, game_state: &GameState);
 }
 
 fn main() {
@@ -54,135 +27,38 @@ fn main() {
 
 	rl.set_target_fps(300);
 
-
-	let hat_texture = rl.load_texture(&thread, "assets/sprites/hat.png").unwrap();
-	let bowtie_texture = rl.load_texture(&thread, "assets/sprites/bowtie.png").unwrap();
-	let pirate_hat_texture = rl.load_texture(&thread, "assets/sprites/pirate_hat.png").unwrap();
-
-	let mut player_1 = Player {
-		pos: Vector2 {
-			x: 630.0,
-			y: 322.0,
-		},
-		width: 20.0,
-		height: 75.0,
-		color: Color { r: 232, g: 190, b: 172, a: 200 },
-		velocity: Vector2 {
-			x: 0.0,
-			y: 0.0
-		},
-		controls: Controls {
-			move_up: KeyboardKey::KEY_W,
-			move_down: KeyboardKey::KEY_S,
-			move_left: KeyboardKey::KEY_A,
-			move_right: KeyboardKey::KEY_D,
-			jump: KeyboardKey::KEY_SPACE,
-		},
-		movement_speed: 800.0,
-		jump_power: 400000.0,
+	let mut game_state = GameState {
+		current_scene: SceneType::Game,
+		assets: Default::default(),
 	};
 
-	let mut player_2 = Player {
-		pos: Vector2 {
-			x: 630.0,
-			y: 322.0,
-		},
-		width: 20.0,
-		height: 75.0,
-		color: Color { r: 232, g: 190, b: 172, a: 200 },
-		velocity: Vector2 {
-			x: 0.0,
-			y: 0.0
-		},
-		controls: Controls {
-			move_up: KeyboardKey::KEY_UP,
-			move_down: KeyboardKey::KEY_DOWN,
-			move_left: KeyboardKey::KEY_LEFT,
-			move_right: KeyboardKey::KEY_RIGHT,
-			jump: KeyboardKey::KEY_RIGHT_CONTROL,
-		},
-		movement_speed: 800.0,
-		jump_power: 400000.0,
-	};
+	game_state.assets.insert(
+		"hat".into(),
+		rl.load_texture(&thread, "assets/sprites/hat.png").unwrap(),
+	);
 
-	let gravity = 2750.0;
+	game_state.assets.insert(
+		"bowtie".into(),
+		rl.load_texture(&thread, "assets/sprites/bowtie.png").unwrap(),
+	);
+
+	game_state.assets.insert(
+		"pirate_hat".into(),
+		rl.load_texture(&thread, "assets/sprites/pirate_hat.png").unwrap(),
+	);
+
+	let mut game_scene = scenes::game::GameScene::default();
+	game_scene.init();
 
 	while !rl.window_should_close() {
 		let delta = rl.get_frame_time();
 		let mut d = rl.begin_drawing(&thread);
 
-		//ground
-		d.draw_rectangle(
-			0,
-			600,
-			1280,
-			120,
-			Color { r: 8, g: 255, b: 65, a: 255 },
-		);
-
-		d.clear_background(Color { r: 43, g: 255, b: 241, a: 255 });
-
-
-		handle_player_movement(&mut player_1, delta, &mut d);
-		handle_player_movement(&mut player_2, delta, &mut d);
-
-		player_1.pos += player_1.velocity * delta;
-		player_1.velocity.y += gravity * delta;
-
-		if player_1.pos.y + player_1.height > 600.0 {
-			player_1.pos.y = 600.0 - player_1.height;
-			player_1.velocity.y = 0.0;
-		}
-
-		player_2.pos += player_2.velocity * delta;
-		player_2.velocity.y += gravity * delta;
-
-		if player_2.pos.y + player_2.height > 600.0 {
-			player_2.pos.y = 600.0 - player_2.height;
-			player_2.velocity.y = 0.0;
-		}
-
-		//players
-		draw_rectangle(&player_1, &mut d);
-		draw_rectangle(&player_2, &mut d);
-
-		//player 1
-		let scale = 0.15;
-		d.draw_texture_ex(
-			&hat_texture,
-			player_1.pos - (Vector2 { x: 500.0, y: 500.0 } / 2.0 * scale) + Vector2 { x: 10.0, y: 0.0 },
-			0.0,
-			scale,
-			Color::WHITE
-		);
-
-		
-		let scale = 0.075;
-		d.draw_texture_ex(
-			&bowtie_texture,
-			player_1.pos - (Vector2 { x: 500.0, y: 500.0 } / 2.0 * scale) + Vector2 { x: 12.0, y: 40.0 },
-			0.0,
-			scale,
-			Color::WHITE
-		);
-
-		// player 2
-		let scale = 0.4;
-		d.draw_texture_ex(
-			&pirate_hat_texture,
-			player_2.pos - (Vector2 { x: 500.0, y: 500.0 } / 2.0 * scale) + Vector2 { x: 8.0, y: 0.0 },
-			0.0,
-			scale,
-			Color::WHITE
-		);
+		match game_state.current_scene {
+			SceneType::Game => {
+				game_scene.update(&mut d, delta);
+				game_scene.display(&mut d, &game_state);
+			}
+		};
 	}
 }
-
-/*
-fn get_screen_size(handle: &mut raylib::RaylibHandle) -> Vector2 {
-	Vector2 {
-		x: handle.get_screen_width() as f32,
-		y: handle.get_screen_height() as f32,
-	}
-}
-*/
---
+++ src/scenes/game.rs
@@ -0,0 +1,165 @@
+use raylib::prelude::*;
+
+use crate::{ GameState, Scene };
+
+struct Player {
+	pos: Vector2,
+	size: Vector2,
+	color:  Color,
+	velocity: Vector2,
+	controls: Controls,
+	movement_speed: f32,
+	jump_power: f32,
+}
+
+struct Controls {
+	move_up: KeyboardKey,
+	move_down: KeyboardKey,
+	move_left: KeyboardKey,
+	move_right: KeyboardKey,
+	jump: KeyboardKey,
+}
+
+fn draw_player(player: &Player, handle: &mut raylib::core::drawing::RaylibDrawHandle) {
+	handle.draw_rectangle_v(
+		player.pos,
+		player.size,
+		player.color,
+	);
+}
+
+fn handle_player_movement(player: &mut Player, delta: f32, handle: &mut raylib::core::drawing::RaylibDrawHandle) {
+	if handle.is_key_pressed(player.controls.jump) {
+		player.velocity.y = -player.jump_power * delta
+	}
+	if handle.is_key_down(player.controls.move_left) {
+		player.pos.x -= player.movement_speed * delta
+	}
+	if handle.is_key_down(player.controls.move_right) {
+		player.pos.x += player.movement_speed * delta
+	}
+}
+
+#[derive(Default)]
+pub struct GameScene {
+	players: Vec<Player>,
+}
+
+const GRAVITY: f32 = 2750.0;
+
+impl Scene for GameScene {
+	fn init(&mut self) {
+		self.players.push(Player {
+			pos: Vector2 {
+				x: 630.0,
+				y: 322.0,
+			},
+			size: Vector2 {
+				x: 20.0,
+				y: 75.0,
+			},
+			color: Color { r: 232, g: 190, b: 172, a: 200 },
+			velocity: Vector2 {
+				x: 0.0,
+				y: 0.0
+			},
+			controls: Controls {
+				move_up: KeyboardKey::KEY_W,
+				move_down: KeyboardKey::KEY_S,
+				move_left: KeyboardKey::KEY_A,
+				move_right: KeyboardKey::KEY_D,
+				jump: KeyboardKey::KEY_SPACE,
+			},
+			movement_speed: 800.0,
+			jump_power: 400000.0,
+		});
+
+		self.players.push(Player {
+			pos: Vector2 {
+				x: 630.0,
+				y: 322.0,
+			},
+			size: Vector2 {
+				x: 20.0,
+				y: 75.0,
+			},
+			color: Color { r: 232, g: 190, b: 172, a: 200 },
+			velocity: Vector2 {
+				x: 0.0,
+				y: 0.0
+			},
+			controls: Controls {
+				move_up: KeyboardKey::KEY_UP,
+				move_down: KeyboardKey::KEY_DOWN,
+				move_left: KeyboardKey::KEY_LEFT,
+				move_right: KeyboardKey::KEY_RIGHT,
+				jump: KeyboardKey::KEY_RIGHT_CONTROL,
+			},
+			movement_speed: 800.0,
+			jump_power: 400000.0,
+		});
+	}
+
+	fn update(&mut self, d: &mut RaylibDrawHandle, delta: f32) {
+		for mut player in self.players.iter_mut() {
+			handle_player_movement(&mut player, delta, d);
+
+			player.pos += player.velocity * delta;
+			player.velocity.y += GRAVITY * delta;
+
+			if player.pos.y + player.size.y > 600.0 {
+				player.pos.y = 600.0 - player.size.y;
+				player.velocity.y = 0.0;
+			}
+		}
+	}
+
+	fn display(&mut self, d: &mut RaylibDrawHandle, game_state: &GameState) {
+		d.clear_background(Color { r: 43, g: 255, b: 241, a: 255 });
+
+		//ground
+		d.draw_rectangle_v(
+			Vector2 { x: 0.0, y: 600.0 },
+			Vector2 { x: 1280.0, y: 120.0 },
+			Color { r: 8, g: 255, b: 65, a: 255 },
+		);
+
+		let player_1 = &self.players[0];
+		let player_2 = &self.players[1];
+
+		//players
+		draw_player(&player_1, d);
+		draw_player(&player_2, d);
+
+		//player 1
+		let scale = 0.15;
+		d.draw_texture_ex(
+			&game_state.assets["hat"],
+			player_1.pos - (Vector2 { x: 500.0, y: 500.0 } / 2.0 * scale) + Vector2 { x: 10.0, y: 0.0 },
+			0.0,
+			scale,
+			Color::WHITE
+		);
+
+
+		let scale = 0.075;
+		d.draw_texture_ex(
+			&game_state.assets["bowtie"],
+			player_1.pos - (Vector2 { x: 500.0, y: 500.0 } / 2.0 * scale) + Vector2 { x: 12.0, y: 40.0 },
+			0.0,
+			scale,
+			Color::WHITE
+		);
+
+		// player 2
+		let scale = 0.4;
+		d.draw_texture_ex(
+			&game_state.assets["pirate_hat"],
+			player_2.pos - (Vector2 { x: 500.0, y: 500.0 } / 2.0 * scale) + Vector2 { x: 8.0, y: 0.0 },
+			0.0,
+			scale,
+			Color::WHITE
+		);
+	}
+
+}
